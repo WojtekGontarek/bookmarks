440d12015aee5422f8f5aacebfd0590f802c18a3
adamkarczewski <akarczewski22@gmail.com>
2024-10-16 08:44:13 +0200

    Kod z 16.10

--- Account/views.py
+++ Account/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from Account.forms import LoginForm
@@ -6,7 +8,20 @@ from Account.forms import LoginForm
 # Create your views here.
 def user_login(request):
     if request.method == 'POST':
-        pass
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data['username'],
+                                password=form.cleaned_data['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Uwierzytelnianie zakończyło się sukcesem.')
+                else:
+                    return HttpResponse('Konto jest zablokowane')
+            else:
+                return HttpResponse('Nieprawidłowe dane')
+
+
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
