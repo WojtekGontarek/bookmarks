dd84fc3cfa65471be88dfca4129bbc5c5f165f0b
student15 <111@111>
2024-12-09 11:45:28 +0100

    Kod z 04.11sdfdsfsasdfsdfadfa

--- images/static/js/bookmarklet.js
+++ images/static/js/bookmarklet.js
@@ -3,7 +3,32 @@ const styleUrl = siteUrl + 'static/css/bookmarklet.css';
 const minWidth = 250
 const minHeight = 250
 
-var head = document.getElementsByTagName('head')[0];
-var link = document.createElement("link");
+const head = document.getElementsByTagName('head')[0];
+const link = document.createElement("link");
 link.rel = "stylesheet";
 link.type = "text/css";
+link.href = siteUrl + '?r=' + Math.floor(Math.random() * 9999999999999);
+head.appendChild(link);
+
+const body = document.getElementsByTagName('body')[0];
+boxHtml = `
+    <div id="bookmarklet">
+        <a href="#" id="close">&times;</a>
+        <h1>Wybierz zdjecie do zapisania:</h1>
+        <div class="images"></div>
+    </div>
+`
+box.innerHTML += boxHtml;
+
+function bookmarkletLaunch(){
+    bookmarklet = document.getElementById('bookmarklet');
+    const imagesFound = document.querySelector(".images");
+    imagesFound.innerHTML = '';
+    bookmarklet.style.display = 'block';
+
+    bookmarklet.getElementById('close').addEventListener('click', function(){
+        bookmarklet.style.display = 'none';
+    });
+}
+
+bookmarkletLaunch();
